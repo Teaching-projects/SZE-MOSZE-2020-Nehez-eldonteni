81dad6abd2ab0b38668c9a3ef325f15738b1d98e
AnonymDavid <jodavid512@gmail.com>
2020-09-14 13:43:41 +0200

    Suggested fixes

--- character.cpp
+++ character.cpp
@@ -1,13 +1,9 @@
 #include "character.h"
- 
+
 
 
 
 std::ostream& operator<<(std::ostream& os, const character& ch) {
 	os << ch.getName() << ": HP: " << ch.getCurrentHP() << ", DMG: " << ch.getAttack() << std::endl;
 	return os;
-}
-
-character::~character()
-{
 }
--- character.h
+++ character.h
@@ -1,28 +1,27 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
- 
+
 #include <iostream>
 
 class character
 {
 public:
-	character(std::string _name, int _maxHP, int _attack) :name(_name), maxHP(_maxHP), attack(_attack)
-	{
-		currentHP = maxHP;
-	}
-	~character();
+	character(std::string _name, int _maxHP, int _attack) :name(_name), maxHP(_maxHP), currentHP(_maxHP), attack(_attack)
+	{}
+
+	~character() {}
 
 	std::string getName() const { return name; }
 	//int getMaxHP() { return maxHP; }
 	int getCurrentHP() const { return currentHP; }
 	int getAttack() const { return attack; }
 
-	bool isDead() { return currentHP == 0; }
+	bool isDead() const { return currentHP == 0; }
 
-	void attackEnemy(character* opponent) { opponent->takeDamage(this); }
-	void takeDamage(character* opponent) { (currentHP - opponent->getAttack() < 0) ? currentHP = 0 : currentHP -= opponent->getAttack(); }
-	
-	friend std::ostream& operator<<(std::ostream& os, const character& ch);
+	void attackEnemy(character& opponent) const { opponent.takeDamage(*this); }
+	void takeDamage(const character& opponent) { (currentHP - opponent.getAttack() < 0) ? currentHP = 0 : currentHP -= opponent.getAttack(); }
+
+	friend std::ostream& operator<<(std::ostream & os, const character & ch);
 
 private:
 	std::string name;
