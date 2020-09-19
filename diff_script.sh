9217f18506989eed1e4959a33ffa89052ed759ec
root <root@DESKTOP-43RO3BA.localdomain>
2020-09-19 13:13:14 +0200

    Small changes + git action

--- diff_script.sh
+++ diff_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-g++ -std=c++17 *.cpp
-# ./a.out units/halis.json units/codos.json
-#diff <(./a.out units/halis.json units/codos.json) <(cat -n dogshit.txt)
 ./a.out units/halis.json units/codos.json > fight.txt
 ./a.out units/halis.json units/kowa.json >> fight.txt
 ./a.out units/codos.json units/halis.json >> fight.txt
@@ -10,10 +7,3 @@ g++ -std=c++17 *.cpp
 ./a.out units/kowa.json units/halis.json >> fight.txt
 ./a.out units/kowa.json units/codos.json >> fight.txt
 
-DIFF=$(diff fight.txt manual_calculation.txt)
-
-if [ "$DIFF" == "" ]; then
-  echo "Successful comparison! No difference between files."
-else
-  echo "Something went wrong! There is a difference."
-fi
