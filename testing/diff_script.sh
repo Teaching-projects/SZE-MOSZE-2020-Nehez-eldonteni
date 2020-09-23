594d0c68569892ff13bfd90fd894781be95aa9cd
haliscsharp <haliscsharp@gmail.com>
2020-09-23 16:07:53 +0200

    Possible bugfix

--- testing/diff_script.sh
+++ testing/diff_script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-touch fight.txt
+
 > fight.txt
+cd ..
 
-./a.out units/halis.json units/codos.json >> fight.txt
-./a.out units/halis.json units/kowa.json >> fight.txt
-./a.out units/codos.json units/halis.json >> fight.txt
-./a.out units/codos.json units/kowa.json >> fight.txt
-./a.out units/kowa.json units/halis.json >> fight.txt
-./a.out units/kowa.json units/codos.json >> fight.txt
+ ./a.out testing/units/halis.json testing/units/codos.json >> testing/fight.txt
+ ./a.out testing/units/halis.json testing/units/kowa.json >> testing/fight.txt
+ ./a.out testing/units/codos.json testing/units/halis.json >> testing/fight.txt
+ ./a.out testing/units/codos.json testing/units/kowa.json >> testing/fight.txt
+ ./a.out testing/units/kowa.json testing/units/halis.json >> testing/fight.txt
+ ./a.out testing/units/kowa.json testing/units/codos.json >> testing/fight.txt
 
-DIFF=$(diff testing/fight.txt testing/manual_calculation.txt)
+ DIFF=$(diff testing/fight.txt testing/manual_calculation.txt)
 
-if [ "$DIFF" == "" ]; then
-	echo "Successful comparison! No difference between files."
-	exit 0
-else
-	echo "Something went wrong! There is a difference."
-	exit 1
-fi
+ if [ "$DIFF" == "" ]; then
+ 	echo "Successful comparison! No difference between files."
+ 	exit 0
+ else
+ 	echo "Something went wrong! There is a difference."
+ 	exit 1
+ fi
