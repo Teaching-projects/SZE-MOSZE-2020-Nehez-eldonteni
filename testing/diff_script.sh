50730de957673abc2c20000e51c03c984f84ac90
nemethvalentin <valentin001998@gmail.com>
2020-09-23 15:36:05 +0200

    Changes in script

--- testing/diff_script.sh
+++ testing/diff_script.sh
@@ -2,14 +2,14 @@
 
 > fight.txt
 
-../a.out units/halis.json units/codos.json >> fight.txt
-../a.out units/halis.json units/kowa.json >> fight.txt
-../a.out units/codos.json units/halis.json >> fight.txt
-../a.out units/codos.json units/kowa.json >> fight.txt
-../a.out units/kowa.json units/halis.json >> fight.txt
-../a.out units/kowa.json units/codos.json >> fight.txt
+./a.out units/halis.json units/codos.json >> fight.txt
+./a.out units/halis.json units/kowa.json >> fight.txt
+./a.out units/codos.json units/halis.json >> fight.txt
+./a.out units/codos.json units/kowa.json >> fight.txt
+./a.out units/kowa.json units/halis.json >> fight.txt
+./a.out units/kowa.json units/codos.json >> fight.txt
 
-DIFF=$(diff fight.txt manual_calculation.txt)
+DIFF=$(diff testing/fight.txt testing/manual_calculation.txt)
 
 if [ "$DIFF" == "" ]; then
 echo "Successful comparison! No difference between files."
