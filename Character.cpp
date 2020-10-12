490aea0581b0fe87b8ffed114b4b08f0bb0a92a8
haliscsharp <haliscsharp@gmail.com>
2020-10-12 14:51:03 +0200

    Added attackspeed system

--- Character.cpp
+++ Character.cpp
@@ -5,10 +5,17 @@
 
 
 std::ostream& operator<<(std::ostream& os, const Character& ch) {
-	os << ch.getName() << ": HP: " << ch.getCurrentHP() << ", DMG: " << ch.getAttack() << std::endl;
+	os << ch.getName() << ": HP: " << ch.getCurrentHP() << ", DMG: " << ch.getAttack() << ", ATKSPEED: " << ch.getAttackspeed() << std::endl;
 	return os;
 }
 
+/**
+	 * \brief A function that returns a character object with the data read from a JSON file given as parameter
+	 * \param fileName
+	 * \return A character object from the input 
+	 * 
+	 * This function provides all the data for the program to process
+	*/
 Character Character::parseUnit(const std::string& fileName)
 {
 	std::ifstream ifsJSON(fileName);
@@ -33,9 +40,47 @@ Character Character::parseUnit(const std::string& fileName)
 	// read dmg
 	std::getline(ifsJSON, line);
 	colonPos = line.find(':');
-	int atk = std::stoi(line.substr(colonPos + 1));
+	int atk = std::stoi(line.substr(colonPos + 2, line.find_last_of(',') - (colonPos + 2)));
+
+	// read attackcooldown
+	std::getline(ifsJSON, line);
+	colonPos = line.find(':');
+	double as = std::stod(line.substr(colonPos + 1));
 
 	ifsJSON.close();
 
-	return Character(name, hp, atk);
+	return Character(name, hp, atk, as);
+}
+/**
+	 * \brief This function makes a character take damage from another character(given as parameter)
+	 * \param Character
+	 * 
+	 * This function is essential for the fight system of the game	 
+	*/
+void Character::takeDamage(Character & opponent) {
+	opponent.currentHP -= this->getAttack();
+	this->currentHP -= opponent.getAttack();
+
+	bool thisAttackOpponent = true;
+
+	while (!(this->isDead()) && !(opponent.isDead()))
+	{
+		if (thisAttackOpponent) {
+			while (!(opponent.isDead()) && this->getCurrentAttackspeed() <= opponent.getCurrentAttackspeed()) {
+				opponent.currentHP -= this->getAttack();
+
+				opponent.currentAttackspeed -= this->getCurrentAttackspeed();
+				this->currentAttackspeed = this->getAttackspeed();
+			}
+		}
+		else {
+			while (!(this->isDead()) && opponent.getCurrentAttackspeed() <= this->getCurrentAttackspeed()) {
+				this->currentHP -= opponent.getAttack();
+
+				this->currentAttackspeed -= opponent.getCurrentAttackspeed();
+				opponent.currentAttackspeed = opponent.getAttackspeed();
+			}
+		}
+		thisAttackOpponent = !thisAttackOpponent;
+	}
 }
--- Character.h
+++ Character.h
@@ -1,3 +1,17 @@
+/**
+ * \class Character
+ * 
+ * \brief Character class
+ * 
+ * \author haliscsharp
+ * 
+ * \version 1.0
+ * 
+ * \date 2020/10/05 23:15
+ * 
+ * Created on 2020/10/05 23:15
+*/
+
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
@@ -7,29 +21,46 @@
 class Character
 {
 public:
-	Character(std::string _name, int _maxHP, int _attack) :name(_name), maxHP(_maxHP), currentHP(_maxHP), attack(_attack)
+	/// Constructor for the class that puts the input parameters into data members
+	Character(std::string _name, int _maxHP, int _attack, double _attackspeed) :name(_name), maxHP(_maxHP), currentHP(_maxHP), attack(_attack), attackspeed(_attackspeed), currentAttackspeed(_attackspeed)
 	{}
 
+	/// Destructor for the class
 	~Character() {}
 
+	/// Simple getter function that returns the name of the character
 	std::string getName() const { return name; }
+	/// Simple getter function that returns the current HP of the character
 	int getCurrentHP() const { return currentHP; }
+	/// Simple getter function that returns the attack damage of the character
 	int getAttack() const { return attack; }
+	/// Simple getter function that returns the time needed between the character's attacks
+	double getAttackspeed() const { return attackspeed; }
+	/// Simple getter function that returns the time remaining for the character's next attack
+	double getCurrentAttackspeed() const { return currentAttackspeed; }
 
-	bool isDead() const { return currentHP == 0; }
+	/// A function that returns if the character is dead(or not)
+	bool isDead() const { return currentHP <= 0; }
 
+	/// A function that parses a unit from a JSON source
 	static Character parseUnit(const std::string& name);
 
-	void attackEnemy(Character& opponent) const { opponent.takeDamage(*this); }
-	void takeDamage(const Character& opponent) { (currentHP - opponent.getAttack() < 0) ? currentHP = 0 : currentHP -= opponent.getAttack(); }
+	/// This functions makes a character attack another character(given as parameter) by calling it's takeDamage method
+	void attackEnemy(Character& opponent) { opponent.takeDamage(*this); }
 
+	/// Makes the output look better
 	friend std::ostream& operator<<(std::ostream & os, const Character & ch);
 
 private:
-	const std::string name;
-	const int maxHP;
-	int currentHP;
-	const int attack;
+	const std::string name;	///< Name of the character
+	const int maxHP;	///< Maximum HP of the character
+	int currentHP;	///< Current HP of the character
+	const int attack;	///< Attack damage of the character
+	const double attackspeed;	///< Time needed between attacks of the character
+	double currentAttackspeed;	///< Time remaining for the character's next attack
+
+	///This function makes a character take damage from another character(given as parameter)
+	void takeDamage(Character& opponent);
 };
 
 #endif
