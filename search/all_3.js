e0f5e58be02fb813fd5bf72f1e6a0c8b9e7dbc7f
haliscsharp <haliscsharp@gmail.com>
2020-10-14 14:22:15 +0000

    Update gh-pages to output generated at 1c8a58b

--- search/all_3.js
+++ search/all_3.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['getattack',['getAttack',['../class_character.html#aaaa4b78668224b53fa593afe0a818cd1',1,'Character']]],
-  ['getattackspeed',['getAttackspeed',['../class_character.html#a0405fa66f7ac4e1a6a7faf1bffe2c5c7',1,'Character']]],
-  ['getcurrentattackspeed',['getCurrentAttackspeed',['../class_character.html#a2250d808977204ac4a5d3a977ef6201e',1,'Character']]],
+  ['getcooldown',['getCooldown',['../class_character.html#a0157dd2c927064b7551b98a9c5a41308',1,'Character']]],
+  ['getcurrentcooldown',['getCurrentCooldown',['../class_character.html#a0f14c32cce50838c74f34f1cdd66ee78',1,'Character']]],
   ['getcurrenthp',['getCurrentHP',['../class_character.html#a678ad66c929f2dd8e5d33663be8a1f7a',1,'Character']]],
   ['getname',['getName',['../class_character.html#ad79f892fe576c7ef0804cad502d94517',1,'Character']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['character',['Character',['../class_character.html#ae232895766588e787d4c169f5b650f8d',1,'Character']]]
+  ['character',['Character',['../class_character.html#ad534c55aa15d730350fa347404683930',1,'Character']]]
 ];
