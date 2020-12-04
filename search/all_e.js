57b2b0dc4d21c38a51c2160e739c9120b2491be8
haliscsharp <haliscsharp@gmail.com>
2020-12-04 16:13:00 +0100

    Update gh-pages to output generated at 0f4091b

--- search/all_e.js
+++ search/all_e.js
@@ -3,5 +3,5 @@ var searchData=
   ['takedamage_93',['takeDamage',['../class_monster.html#a15d49c5ebaee321e856ce6ba9614ca4c',1,'Monster']]],
   ['textrenderer_94',['TextRenderer',['../class_text_renderer.html',1,'TextRenderer'],['../class_text_renderer.html#a2ffb5baddcc264fbaccda8fb7c660814',1,'TextRenderer::TextRenderer()'],['../class_text_renderer.html#a6dc859766948672e9c3af62d2d9ab9d2',1,'TextRenderer::TextRenderer(std::ostream &amp;os)'],['../class_text_renderer.html#ab90cd29b3e02cfaf5ab753c74608040c',1,'TextRenderer::TextRenderer(std::ostream *os)']]],
   ['texture_95',['texture',['../class_monster.html#aefc60af296cf019f3ddceb0cf6093efc',1,'Monster']]],
-  ['type_96',['type',['../struct_marked_map_1_1unit_pos.html#a2aa10d35bdb9221f69d2b247cde37859',1,'MarkedMap::unitPos']]]
+  ['type_96',['type',['../class_map.html#a41697b848ea9a5aad1910714e0196b7b',1,'Map::type()'],['../struct_marked_map_1_1unit_pos.html#a2aa10d35bdb9221f69d2b247cde37859',1,'MarkedMap::unitPos::type()']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -4,8 +4,9 @@ var indexSectionsWithContent =
   1: "acdghjmnoprstuw",
   2: "acdfghimnoprstw~",
   3: "acdfgimnoprtw",
-  4: "o",
-  5: "n"
+  4: "t",
+  5: "o",
+  6: "n"
 };
 
 var indexSectionNames =
@@ -14,8 +15,9 @@ var indexSectionNames =
   1: "classes",
   2: "functions",
   3: "variables",
-  4: "related",
-  5: "pages"
+  4: "enums",
+  5: "related",
+  6: "pages"
 };
 
 var indexSectionLabels =
@@ -24,7 +26,8 @@ var indexSectionLabels =
   1: "Classes",
   2: "Functions",
   3: "Variables",
-  4: "Friends",
-  5: "Pages"
+  4: "Enumerations",
+  5: "Friends",
+  6: "Pages"
 };
 
